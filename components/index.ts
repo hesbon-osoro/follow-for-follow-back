2477a96c804a257bc1d4c35c6ddd307a0d281675
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-11-21 23:54:33 +0300

    components exporter

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+export { default as Followers } from './Followers';
+export { default as Following } from './Following';
+export { default as Repositories } from './Repositories';
+export { default as Login } from './Login';
+export { default as DropLet } from './DropLet';
+export { default as Profile } from './Profile';
