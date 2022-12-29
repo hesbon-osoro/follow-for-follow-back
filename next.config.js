7e7ca645eb6dc642aad15e933fe30ff37ed3f376
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-12-29 17:58:18 +0300

    update image domain

--- next.config.js
+++ next.config.js
@@ -3,11 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['https://avatars.githubusercontent.com'],
+    domains: ['https://avatars.githubusercontent.com', 'restcountries.eu'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'restcountries.eu',
         pathname: '/**',
       },
     ],
