4b19bec31c8feed66e45cee12a61ea27b4e34600
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-12-29 18:20:12 +0300

    updating helper util functions

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,6 @@
 export { getElapsedTime } from './getElapsedTime';
 export { thousandSeparator } from './thousandSeparator';
+export { extractCityState } from './extractCityState';
+export { trimExtraSpaces } from './trimExtraSpaces';
+export { default as countriesAndCapitals } from './countryCapital.json';
+export { countryCode, capitalCode } from './countryCode';
