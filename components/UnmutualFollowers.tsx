30f0bab3f05819ace84baadb6d6b8a57f36768c4
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-12-29 18:09:09 +0300

    UnmutualFollowers.tsx

---
+++ components/UnmutualFollowers.tsx
@@ -0,0 +1,104 @@
+//@ts-nocheck
+import React, { FC, useEffect, useState } from 'react';
+import Pagination from 'react-paginate';
+import Image from 'next/image';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+
+let followers = [];
+let following = [];
+
+import DropLet from './DropLet';
+
+const UnmutualFollowers: FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  if (typeof window !== 'undefined') {
+    followers = JSON.parse(localStorage.getItem('followers'));
+    following = JSON.parse(localStorage.getItem('following'));
+  }
+
+  const toggleLoading = () => {
+    setLoading(!loading);
+  };
+
+  useEffect(() => {
+    setTimeout(toggleLoading, 1500);
+  }, []);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+
+  if (loading) {
+    return <DropLet />;
+  }
+
+  const UnmutualFollowers = followers.filter(
+    user => !following.some(follower => follower.id === user.id)
+  );
+
+  const followersOnCurrentPage = UnmutualFollowers.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
+  console.log(UnmutualFollowers.length);
+  return (
+    <div className="following">
+      <div className="table">
+        <h2>Unmutual Followers</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Avatar</th>
+              <th>Username</th>
+              <th>Follow</th>
+            </tr>
+          </thead>
+          <tbody>
+            {followersOnCurrentPage.map((follower, index) => (
+              <tr key={follower.id}>
+                <td>{(currentPage - 1) * pageSize + index + 1}</td>
+                <td>
+                  <Image
+                    src={follower.avatar_url}
+                    width={50}
+                    height={50}
+                    alt={follower.login}
+                  />
+                </td>
+                <td>{follower.login}</td>
+                <td>
+                  <a
+                    href={follower.html_url}
+                    rel="noreferrer noopener"
+                    target="_blank"
+                  >
+                    Follow
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="buttons">
+        <Pagination
+          pageCount={Math.ceil(UnmutualFollowers.length / pageSize)}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={2}
+          onPageChange={({ selected }) => setCurrentPage(selected + 1)}
+          containerClassName="pagination"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          activeClassName="active"
+          previousLabel={<FaArrowLeft />}
+          nextLabel={<FaArrowRight />}
+          breakLabel={<span className="dots">...</span>}
+          disabledClassName="disabled"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UnmutualFollowers;
