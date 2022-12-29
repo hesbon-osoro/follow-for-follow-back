c6c50c1264ec71880fb60ae0c31bdebc132ae759
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-12-29 18:07:03 +0300

    update components export

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,11 @@
 export { default as Followers } from './Followers';
 export { default as Following } from './Following';
-export { default as Repositories } from './Repositories';
-export { default as Login } from './Login';
 export { default as DropLet } from './DropLet';
 export { default as Profile } from './Profile';
+export { default as UnmutualFollowing } from './UnmutualFollowing';
+export { default as FollowingCard } from './FollowingCard';
+export { default as FollowersCard } from './FollowersCard';
+export { default as UnmutualFollowers } from './UnmutualFollowers';
+export { default as MenuButton } from './MenuButton';
+export { default as SetUsernameModal } from './SetUsernameModal';
+export { default as ErrorBoundary } from './ErrorBoundary';
