50941db4ce408ea9a286e8d44bb29b3d06d9c6ec
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-12-30 22:55:02 +0300

    SEO improvement

--- components/Head.tsx
+++ components/Head.tsx
@@ -5,7 +5,11 @@ const Head = ({ title = 'Follow For Follow Back' }: { title: string }) => (
     <title>{title}</title>
     <meta
       name="description"
-      content="Find your mutual connections on GitHub with Follow for Follow back."
+      content="Find your mutual connections on GitHub with Follow for Follow back. Increase your followers and following with this tool."
+    />
+    <meta
+      name="keywords"
+      content="GitHub, mutual connections, followers, following, social media, networking, developer community"
     />
     <link rel="icon" href="./fffb.png" />
   </HeadTag>
